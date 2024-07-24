75f14ad29aa9fdbecdb92c50534a13c455e5f7e7
lunqwe <roma.boyko10068@gmail.com>
2024-07-24 18:51:54 +0300

    added required installed app

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,9 @@ INSTALLED_APPS = [
     
     #side apps
     'rest_framework',
+    'rest_framework.authtoken',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
     
     #local apps
