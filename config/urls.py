59748654594af85017dcbde8cb747a844a3d9dbe
lunqwe <roma.boyko10068@gmail.com>
2024-07-24 18:47:18 +0300

    simple-jwt urls setup

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 #drf-yasg setup
 schema_view = get_schema_view(
@@ -38,6 +42,10 @@ urlpatterns = [
     #drf-yasg
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    
+    #simple-jwt
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
     #local
     path('api/v1/accounts/', include('accounts.urls')),
