ef139f0a1aa354bb740fd6bfa7f8a73e5efee9c5
lunqwe <roma.boyko10068@gmail.com>
2024-07-25 19:56:27 +0300

    utils description

--- posts/utils.py
+++ posts/utils.py
@@ -1,13 +1,23 @@
 from transformers import BertForSequenceClassification, BertTokenizer, TextClassificationPipeline
+""" 
+This file is about using Hugging Face models to create required project functionality
+
+Models:
+1. https://huggingface.co/JungleLee/bert-toxic-comment-classification
+
+I used this model to check posts and comments for obscene text (check_for_obscence function)
+
+2. 
+
+"""
 
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 model = BertForSequenceClassification.from_pretrained("JungleLee/bert-toxic-comment-classification")
 
 pipeline = TextClassificationPipeline(model=model, tokenizer=tokenizer)
 
-def check_for_obscence(text:str) -> bool:
+def check_for_obscence(text: str) -> bool:
+    
+    
     is_toxic = pipeline(text)[0].get('label')
-    if is_toxic == 'toxic':
-        return False
-    else:
-        return True
+    return is_toxic != 'toxic'
