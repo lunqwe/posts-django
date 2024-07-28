096fbc31f3d82b9f2a34ebd3a7d55317141fe4ce
lunqwe <roma.boyko10068@gmail.com>
2024-07-28 18:45:24 +0300

    updated filters

--- posts/filters.py
+++ posts/filters.py
@@ -1,10 +1,12 @@
-from django_filters import FilterSet
+from django_filters import FilterSet, DateFilter
 from .models import Post
 
 class PostFilter(FilterSet):
+    date_from = DateFilter(field_name='created_at', lookup_expr='gte')
+    date_to = DateFilter(field_name='created_at', lookup_expr='lte')
     class Meta:
         model = Post
-        fields = {
-            'text': ['exact', 'icontains'],
-            'owner': ['exact']
-        }
+        fields = {'text': ['icontains', 'exact'],
+                  'owner': ['exact'],
+                  }
+        extra_fields = ['date_from', 'date_to']
